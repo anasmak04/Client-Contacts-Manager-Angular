aff602b8633d52c25c10d709086dac68576ab054
anasmak04 <anasdev8@gmail.com>
2022-10-12 20:34:20 +0100

    add components

--- Client-Contacts-Manager-Angular/src/app/app.module.ts
+++ Client-Contacts-Manager-Angular/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClientFormComponent } from './clients/client-form/client-form.component';
+import { ClientPageComponent } from './clients/client-page/client-page.component';
+import { SearchFormComponent } from './search/search-form/search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientFormComponent,
+    ClientPageComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule
---
+++ Client-Contacts-Manager-Angular/src/app/clients/client-form/client-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-client-form',
+  templateUrl: './client-form.component.html',
+  styleUrls: ['./client-form.component.css']
+})
+export class ClientFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
