2bf6efa2851225c2765228c76536408bce2f9a3a
anasmak04 <anasdev8@gmail.com>
2022-10-13 14:27:43 +0100

    add sub models

--- Client-Contacts-Manager-Angular/src/app/clients/client-form/client-form.component.ts
+++ Client-Contacts-Manager-Angular/src/app/clients/client-form/client-form.component.ts
@@ -1,5 +1,11 @@
 import { Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
-
+import {
+  Form,
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators
+} from '@angular/forms';
 @Component({
   selector: 'app-client-form',
   templateUrl: './client-form.component.html',
@@ -7,11 +13,32 @@ import { Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
 })
 export class ClientFormComponent implements OnInit {
 
-  
+  // new FormGroup is defined here
+  clientForm: FormGroup;
 
-  constructor() { }
+  // creating new FormControls, with validation
+  firstname = new FormControl('', Validators.required);
+  lastname = new FormControl('', Validators.required);
+  email = new FormControl('', Validators.required);
+  telephoneNumber = new FormControl('');
+  companyName = new FormControl('', Validators.required);
+
+  constructor(fb: FormBuilder) {
+    this.clientForm = fb.group({
+      firstname: this.firstname,
+      lastname: this.lastname,
+      email: this.email,
+      telephoneNumber: this.telephoneNumber,
+      companyName: this.companyName
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  // event called when form is submitted, displaying the output of the form
+  saveClient() {
+    console.log(this.clientForm.value);
   }
 
 }
