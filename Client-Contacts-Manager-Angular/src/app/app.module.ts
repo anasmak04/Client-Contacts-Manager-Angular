cd191ef1fb5b6042024b38a862f5824410700015
anasmak04 <anasdev8@gmail.com>
2022-10-13 08:53:26 +0100

    add client-form template

--- Client-Contacts-Manager-Angular/src/app/app.module.ts
+++ Client-Contacts-Manager-Angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from "@angular/forms";
+import { FormsModule ,ReactiveFormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { ClientFormComponent } from './clients/client-form/client-form.component';
@@ -18,7 +18,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
