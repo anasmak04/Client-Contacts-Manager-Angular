ffa4a02a1c17a5aee8e245d235f23f1294fae4d4
anasmak04 <anasdev8@gmail.com>
2022-10-13 16:34:03 +0100

    add company component inside routing module

--- Client-Contacts-Manager-Angular/src/app/company/company-module/company-module.module.ts
+++ Client-Contacts-Manager-Angular/src/app/company/company-module/company-module.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { CompanyPageComponent } from '../company-page/company-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CompanyPageComponent
+  ],
   imports: [
     CommonModule
   ]
